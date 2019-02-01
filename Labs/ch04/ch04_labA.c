f151ea6de31926ad63dea2e50aba9b1f887e208f
Ogden101 <grant101.ogden@gmail.com>
2019-02-01 16:59:01 -0600

    Commented ch04_labA.c

--- Labs/ch04/ch04_labA.c
+++ Labs/ch04/ch04_labA.c
@@ -1,12 +1,21 @@
+/*
+    Programmer: Grant Ogden
+    Date: 2/1/2019
+    Filename: ch04_labA.c
+    Reason: To practice implentation of getchar() and putchar()
+*/
 #include <stdio.h>
 
 int main()
 {
+    //user input
     char input;
     
     printf("Please input your character to be incremented: ");
+    //recieve the users input
     input = getchar();
     printf("The next character is.....");
+    //print the desired answer by adding one
     putchar(input + 1);
     printf("\n");
 
