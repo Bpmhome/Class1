23e66e26b3c3dd3354d2109546e87d11eb2b2f04
Ogden101 <grant101.ogden@gmail.com>
2019-02-08 09:10:45 -0600

    labC7.2.c has been commented and labD7.2.c has been finished & commented

--- Labs/ch07/labC7.2.c
+++ Labs/ch07/labC7.2.c
@@ -8,17 +8,21 @@
 
 int main()
 {
+    //our three unsigned integers
     unsigned int var1 = 0;
     unsigned int var2 = 0;
     unsigned int var3 = 0;
 
+    //gotta get that sweet input
     printf("Please enter two positive numbers seperated by a , : ");
     scanf("%u,%u",&var1,&var2);
 
+    //if they are equal, well yeah narwhals
     if(var1 == var2)
     {
         printf("ERROR CODE: Narwhal\n");
     }
+    //choose which is greater then clone it?
     else if(var1 > var2)
     {
         var3 = var1;
@@ -28,6 +32,7 @@ int main()
         var3 = var2;
     }
 
+    //if the user was creative enough to not put two ones then tell them what they entered
     if((var1 + var2 + var3) > 2)
     {
         printf("var1 = %d\nvar2 = %d\nvar3 = %d\n",var1,var2,var3);
