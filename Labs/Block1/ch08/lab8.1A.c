e8dde41a34749e8a1ef7abbc583b086bfa3a7a4a
Ogden101 <grant101.ogden@gmail.com>
2019-02-14 10:31:17 -0600

    recovering from git fiasco

--- Labs/Block1/ch08/lab8.1A.c
+++ Labs/Block1/ch08/lab8.1A.c
@@ -14,29 +14,51 @@
 
 // Create your prototypes here
 int replace_character(char * string, const char findThisChar,const char replaceItWithThis);
-int clearInput();    // I included an awesome function used to clear your input buffer
+int clear_input();    // I included an awesome function used to clear your input buffer
 
 int main(void)
 {
-	/* FUNCTION INPUT ARRAYS 
-    ---These are included because we have not learned about pointers yet
-    */
-	char beforeString1[] = { "If debugging is the process of removing software bugs, then programming must be the process of putting them in.  -Edsger Dijkstra" }; // No newlines
-	char * beforeStringArray[] = { beforeString1 };
+	//Variable Declerations
+	char beforeString1[300] = {0};                  // Array to hold user input
+	char * beforeStringArray[] = { beforeString1 }; // Pointer Array to hold array addresses
+    int errorCatcher = 1;                           // Integer to catch the output from the replace_character function to check for errors
+    char oldChar = 0;                               // Char var to hold what the user wants changed
+    char newChar = 0;                               // Char var to hold what the user wants changed to
 
-    // TODO: Create your main variables
+    //Get the user's string
+    printf("Please input the string you want to modify:\n");
+    fscanf(stdin,"%299[^\n]c",beforeString1);
+    //Clear the stdin Buffer
+    clear_input();
+    //Get the value that the user wants changed and what to change it to
+    printf("Please input the character you want replaced then the new character seperated by a comma (t,e):");
+    fscanf(stdin,"%c,%c",&oldChar,&newChar);
 
+    //Pass to the replace_character function and catch the return
+    errorCatcher = replace_character( *beforeStringArray,oldChar,newChar);
 
-    // TODO: Create your program output, user instructions, user input etc
+    //Check the return to see if there was an error
+    switch (errorCatcher)
+    {
+        case 0:
+            printf("Error: None Found\n");
+            break;
+        case -1:
+            printf("Error: Null Pointer\n");
+            break;
+        case -2:
+            printf("Error: Find Equals Replace\n");
+            break;
+        case -3:
+            printf("Error: Non-Printable Char\n");
     
-
-    // TODO: Implement replace_char function call here
-
-
-    // TODO: Check for error return, handle it
+        default:
+            break;
+    }
+    //print the new string to the user
+    printf("%s", beforeString1);
+    printf("\n");
     
-
-
 	return 0;
 }
 
@@ -50,7 +72,7 @@ int main(void)
  *
  * RETURNS:	   Number of characers replaced
  *             ERR_NULL_POINTER if buffer == NULL
- *             ERR_NONE_FOUND if no newlines are found
+ *             ERR_NONE_FOUND if no finds are found
  *             ERR_FIND_EQUALS_REPLACE -2              // Error:  To replace and replace are equal to each other
  *             ERR_NON_PRINTABLE_CHARACTER -3          // Error:  One of the characters is not printable
  *
@@ -59,41 +81,35 @@ int main(void)
 // TODO: Create your function here
 int replace_character(char * string, const char findThisChar,const char replaceItWithThis)
 {
-    int counter = 0;
-    int numberOfChanges = 0;
+    int counter = 0;            //counts the positions through the string
+    int numberOfChanges = 0;    //counts the number of changes
 
-    if(string == NULL)
+    //checking for errors
+    if(string == NULL)                              //If the string pointer is null
     {
         return ERR_NULL_POINTER;
     }
-    else if(findThisChar == replaceItWithThis)
+    else if(findThisChar == replaceItWithThis)      //If the find and replace variables are the same
     {
         return ERR_FIND_EQUALS_REPLACE;
     }
-    else if((findThisChar >= 32 && findThisChar <= 126) || (replaceItWithThis >= 32 && replaceItWithThis <= 126))
+    else if(!((findThisChar >= 32 && findThisChar <= 126) || (replaceItWithThis >= 32 && replaceItWithThis <= 126)))    //if either are nonprintable characters
     {
         return ERR_NON_PRINTABLE_CHARACTER;
     }
-    else
-    {
-        for(int i = 0; i < sizeof(string); i++)
-        {
-            if(string[i] == '\0')
-            {
-                break;
-            }
-        }
-        return ERR_NONE_FOUND;
-    }
 
-    while(string[counter] != '\0')
+    while(string[counter] != '\0')              //while the char isn't null
     {
-        if(string[counter] == findThisChar)
+        if(string[counter] == findThisChar)     //if the char is the one were are looking for
         {
-            string[counter] = replaceItWithThis;
-            numberOfChanges++;
+            string[counter] = replaceItWithThis;//put the new char into that position
+            numberOfChanges++;                  //count that change!!
         }
-        counter++;
+        counter++;                              //next char please
+    }
+    if(numberOfChanges == 0)                    //if we didn't change anything then what the hell
+    {
+        return ERR_NONE_FOUND;
     }
     return numberOfChanges;
 }
@@ -101,7 +117,7 @@ int replace_character(char * string, const char findThisChar,const char replaceI
 
 
 // This is an awesome function and useful if you need to clear the input buffer
-int clearInput()
+int clear_input()
 {
     while ( getchar() != '\n' );
 }
