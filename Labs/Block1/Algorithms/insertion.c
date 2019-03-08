f6b8353421e90f69d25993b39ee2f39449d404b4
Ogden101 <grant101.ogden@gmail.com>
2019-03-08 09:34:33 -0600

    Um yeah got it started

---
+++ Labs/Block1/Algorithms/insertion.c
@@ -0,0 +1,29 @@
+/*
+    Ummmm..... Insertion sort.... So..... Yeah....
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE 2
+
+int main()
+{
+    int theNumbers[ARRAY_SIZE] = {0,1};
+    char* value = "hello";
+    //srand()
+
+    //for(size_t i = 0; i < ARRAY_SIZE; i++)
+    //{
+    //    theNumbers[i] = 
+    //}
+    if(theNumbers[0]= 2)
+    {
+        printf("%d\n",theNumbers[0]);
+    }
+    printf("%c\n",value);
+    printf("%s\n", theNumbers[0] == 0 ? "correct" : "incorrect");
+    
+
+    return 0;
+}
